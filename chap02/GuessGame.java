d78132bfc7675ac98ae682a28e6be9bca6508a04
sachin-mahawar <sachin.a.mahawar@accenture.com>
2025-01-22 08:54:10 +0000

    1. Added name to Player class. 2. Changed GuessGame class to show names of player.

--- chap02/GuessGame.java
+++ chap02/GuessGame.java
@@ -7,9 +7,9 @@ public class GuessGame {
    Player p3;
    
    public void startGame() {
-       p1 = new Player();
-       p2 = new Player();
-       p3 = new Player();
+       p1 = new Player("A");
+       p2 = new Player("B");
+       p3 = new Player("C");
        int guessp1 = 0;
        int guessp2 = 0;
        int guessp3 = 0;
@@ -18,6 +18,7 @@ public class GuessGame {
        boolean p3isRight = false;
        int targetNumber = (int) (Math.random() * 10);
        System.out.println("I'm thinking of a number between 0 and 9...");
+
        while(true) {
            System.out.println("Number to guess is " + targetNumber);
            
@@ -26,11 +27,11 @@ public class GuessGame {
            p3.guess();
            
            guessp1 = p1.number;
-           System.out.println("Player one guessed " + guessp1);
+           System.out.println("Player " + p1.name + " guessed " + guessp1);
            guessp2 = p2.number;
-           System.out.println("Player two guessed " + guessp2);
+           System.out.println("Player " + p2.name + " guessed " + guessp2);
            guessp3 = p3.number;
-           System.out.println("Player three guessed " + guessp3);
+           System.out.println("Player " + p3.name + " guessed " + guessp3);
            
            if (guessp1 == targetNumber) {
                p1isRight = true;
@@ -45,9 +46,9 @@ public class GuessGame {
            if (p1isRight || p2isRight || p3isRight)
            {
                System.out.println("We have a winner!");
-               System.out.println("Player one got it right? " + p1isRight);
-               System.out.println("Player two got it right? " + p2isRight);
-               System.out.println("Player three got it right? " + p3isRight);
+               System.out.println("Player " + p1.name + " got it right? " + p1isRight);
+               System.out.println("Player " + p2.name + " got it right? " + p2isRight);
+               System.out.println("Player " + p3.name + " got it right? " + p3isRight);
                System.out.println("Game is over");
                break;
            }
--- chap02/Player.java
+++ chap02/Player.java
@@ -2,6 +2,12 @@ package chap02;
 
 public class Player {
     int number = 0;
+    String name = "";
+
+    Player(String name){
+        this.name = name;
+    }
+
     public void guess()
     {
         number = (int) (Math.random() * 10);
